1e7e296a1662136f802dcad0f71e6e0939319518
JSterling8 <JSterling8@gmail.com>
2015-12-04 12:59:33 +0000

    Added a listener to topic user being removed.  Not yet using it anywhere yet

---
+++ src/listeners/TopicUserRemovedRemoteEventListener.java
@@ -0,0 +1,44 @@
+package listeners;
+
+import java.rmi.RemoteException;
+import java.util.UUID;
+
+import javax.swing.table.DefaultTableModel;
+
+import controllers.ChatroomController;
+import models.JMSTopicUserRemoved;
+import net.jini.core.event.RemoteEvent;
+import net.jini.core.event.RemoteEventListener;
+import net.jini.core.event.UnknownEventException;
+import net.jini.space.AvailabilityEvent;
+
+public class TopicUserRemovedRemoteEventListener implements RemoteEventListener {
+	private ChatroomController controller;
+	
+	public TopicUserRemovedRemoteEventListener(ChatroomController controller) {
+		this.controller = controller;
+	}
+	
+	@Override
+	public void notify(RemoteEvent remoteEvent) throws UnknownEventException, RemoteException {
+		try {
+			AvailabilityEvent availEvent = (AvailabilityEvent) remoteEvent;
+			JMSTopicUserRemoved topicUserRemoved = (JMSTopicUserRemoved) availEvent.getEntry();
+			
+			DefaultTableModel usersTableModel = controller.getUsersTableModel();
+			
+			for(int i = 0; i < usersTableModel.getRowCount(); i++){
+				UUID idOfUserInTable = (UUID) usersTableModel.getValueAt(i, 1);
+				if (idOfUserInTable.equals(topicUserRemoved.getTopicUser().getUser().getId())){
+					usersTableModel.removeRow(i);
+					
+					break;
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
