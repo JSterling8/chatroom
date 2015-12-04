ff0689cd25e69f94f0f86ab397edda02342c98d2
JSterling8 <JSterling8@gmail.com>
2015-12-04 13:17:49 +0000

    Removed completed TODO and obsolete commented out line of code

--- src/controllers/ProgramController.java
+++ src/controllers/ProgramController.java
@@ -5,11 +5,9 @@ import javax.swing.SwingUtilities;
 import views.LoginFrame;
 
 //FIXME - Makes views resize better
-//FIXME - Handle users leaving topics
 //FIXME - Handle topic deletions (remove them from main menu and kick all users?)
 public class ProgramController {
 	public static void main(String[] args) throws InterruptedException {
-		// SpaceService.addCodeBaseFor(MessageRemoteEventListener.class);
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				new LoginFrame();
