c13e49f773e474e908e83b1f60c1d1b56b1e3cf1
JSterling8 <JSterling8@gmail.com>
2015-12-13 22:16:15 +0000

    Added JavaDoc to ProgramController

--- src/controllers/ProgramController.java
+++ src/controllers/ProgramController.java
@@ -4,6 +4,13 @@ import javax.swing.SwingUtilities;
 
 import views.LoginFrame;
 
+/**
+ * 
+ * Runs the chatroom application.
+ * 
+ * @author Jonathan Sterling
+ *
+ */
 public class ProgramController {
 	public static void main(String[] args) throws InterruptedException {
 		SwingUtilities.invokeLater(new Runnable() {
