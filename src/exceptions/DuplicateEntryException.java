1aa194bc5d6e7f66e5cd2af4e164ecd9349390ec
JSterling8 <JSterling8@gmail.com>
2015-12-13 22:17:43 +0000

    Added JavaDoc to exceptions

--- src/exceptions/DuplicateEntryException.java
+++ src/exceptions/DuplicateEntryException.java
@@ -1,11 +1,19 @@
 package exceptions;
 
+/**
+ * An exception for use when a user attempts to add a duplicate Entry where only
+ * unique entries are permitted
+ * 
+ * @author Jonathan Sterling
+ *
+ */
 public class DuplicateEntryException extends Exception {
 	private static final long serialVersionUID = -7972952713023483444L;
 
 	public DuplicateEntryException() {
-		super();}
-	
+		super();
+	}
+
 	public DuplicateEntryException(String message) {
 		super(message);
 	}
--- src/exceptions/ResourceNotFoundException.java
+++ src/exceptions/ResourceNotFoundException.java
@@ -1,9 +1,16 @@
 package exceptions;
 
+/**
+ * An exception for use when a user attempts to access an Entry that does not
+ * exist in the space.
+ * 
+ * @author Jonathan Sterling
+ *
+ */
 public class ResourceNotFoundException extends Exception {
 	private static final long serialVersionUID = -7276934995576677248L;
 
-	public ResourceNotFoundException(String message){
+	public ResourceNotFoundException(String message) {
 		super(message);
 	}
 }
