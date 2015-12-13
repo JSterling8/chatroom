82114c7e39d67659ef133b9bcb050810a279f6ec
JSterling8 <JSterling8@gmail.com>
2015-12-13 22:26:53 +0000

    Added JavaDoc for TopicAddedRemoteEventListener

--- src/listeners/TopicAddedRemoteEventListener.java
+++ src/listeners/TopicAddedRemoteEventListener.java
@@ -8,6 +8,12 @@ import net.jini.core.event.RemoteEvent;
 import net.jini.core.event.RemoteEventListener;
 import net.jini.space.AvailabilityEvent;
 
+/**
+ * Listens for a topic being added to the space for a given MainMenuController.
+ * 
+ * @author Jonathan Sterling
+ *
+ */
 public class TopicAddedRemoteEventListener implements RemoteEventListener, Serializable {
 	private static final long serialVersionUID = -2085393418959111560L;
 	
@@ -19,17 +25,22 @@ public class TopicAddedRemoteEventListener implements RemoteEventListener, Seria
 		this.controller = controller;
 	}
 	
+	/**
+	 * Listens for a topic being added to the space for a given MainMenuController.
+	 */
 	public void notify(RemoteEvent event) {
 		try {
+			// Get the topic added that triggered the notification
 			AvailabilityEvent availEvent = (AvailabilityEvent) event;
 			JMSTopic topic = (JMSTopic) availEvent.getEntry();
 			
+			// Add the topic data to the MainMenuController's topic list.
 			Object[] rowData = { topic.getName(), topic.getOwner().getName(),
 					topic.getOwner().getId(), topic.getId() };
 			
 			controller.getTopicTableModel().addRow(rowData);
 		} catch (Exception e) {
-			System.err.println("Failed to run notify method for Topics");
+			System.err.println("Failed to run notify method for Topic Creation");
 			e.printStackTrace();
 		}
 	}
