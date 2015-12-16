0ac07ae4b7f38f9e142929d0ff7cb7b5c55a49f0
JSterling8 <JSterling8@gmail.com>
2015-12-16 13:24:09 +0000

    Added JavaDoc to ColoredTable

--- src/views/ColoredTable.java
+++ src/views/ColoredTable.java
@@ -9,33 +9,34 @@ import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableModel;
 
-public class ColoredTable extends JTable
-{
+/**
+ * A JTable extension for easily colouring table rows
+ * 
+ * @author Jonathan Stering
+ *
+ */
+public class ColoredTable extends JTable {
 	private static final long serialVersionUID = -1118415077892936053L;
-	
-	private Map<Integer, Color> rowColor = new HashMap<Integer, Color>();
 
-     public ColoredTable(TableModel model)
-     {
-          super(model);
-     }
+	private Map<Integer, Color> rowColors = new HashMap<Integer, Color>();
 
-     @Override
-     public Component prepareRenderer(TableCellRenderer renderer, int row, int column)
-     {
-          Component c = super.prepareRenderer(renderer, row, column);
+	public ColoredTable(TableModel model) {
+		super(model);
+	}
 
-          if (!isRowSelected(row))
-          {
-               Color color = rowColor.get( row );
-               c.setBackground(color == null ? getBackground() : color);
-          }
+	@Override
+	public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+		Component c = super.prepareRenderer(renderer, row, column);
 
-          return c;
-     }
+		if (!isRowSelected(row)) {
+			Color color = rowColors.get(row);
+			c.setBackground(color == null ? getBackground() : color);
+		}
 
-     public void setRowColor(int row, Color color)
-     {
-          rowColor.put(row, color);
-     }
+		return c;
+	}
+
+	public void setRowColor(int row, Color color) {
+		rowColors.put(row, color);
+	}
 }
