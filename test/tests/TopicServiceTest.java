77bc48f1d02c4646869340cd482a1f5a1b8054a9
JSterling8 <JSterling8@gmail.com>
2015-12-16 20:48:41 +0000

    Finished TopicServiceTest tests

--- test/tests/TopicServiceTest.java
+++ test/tests/TopicServiceTest.java
@@ -1,8 +1,6 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.nio.file.AccessDeniedException;
 import java.rmi.RemoteException;
@@ -24,6 +22,7 @@ import models.JMSMessage;
 import models.JMSTopic;
 import models.JMSTopicUser;
 import models.JMSUser;
+import net.jini.core.entry.UnusableEntryException;
 import net.jini.core.lease.Lease;
 import net.jini.core.lease.UnknownLeaseException;
 import net.jini.core.transaction.TransactionException;
@@ -113,9 +112,9 @@ public class TopicServiceTest {
 		// existing name. With 50 characters, the collision chance is
 		// 1 / (26^50)
 
-		String randomTopicName = RandomStringUtils.randomAlphabetic(50);
-		JMSTopic topic = new JMSTopic(randomTopicName, aUser);
-		JMSTopic topicDupe = new JMSTopic(randomTopicName, aUser);
+		String randomName = RandomStringUtils.randomAlphabetic(50);
+		JMSTopic topic = new JMSTopic(randomName, aUser);
+		JMSTopic topicDupe = new JMSTopic(randomName, aUser);
 		boolean expectedExceptionThrown = false;
 
 		try {
@@ -138,9 +137,9 @@ public class TopicServiceTest {
 		// existing name. With 50 characters, the collision chance is
 		// 1 / (26^50)
 
-		String randomTopicName = RandomStringUtils.randomAlphabetic(50);
-		JMSTopic topic = new JMSTopic(randomTopicName.toLowerCase(), aUser);
-		JMSTopic topicDupe = new JMSTopic(randomTopicName.toUpperCase(), aUser);
+		String randomName = RandomStringUtils.randomAlphabetic(50);
+		JMSTopic topic = new JMSTopic(randomName.toLowerCase(), aUser);
+		JMSTopic topicDupe = new JMSTopic(randomName.toUpperCase(), aUser);
 		boolean expectedExceptionThrown = false;
 
 		try {
@@ -162,10 +161,9 @@ public class TopicServiceTest {
 		// Using a 50 character random name here to avoid collision with
 		// existing name. With 50 characters, the collision chance is
 		// 1 / (26^50)
-		String randomTopicName = RandomStringUtils.randomAlphabetic(50);
-		JMSTopic topic = new JMSTopic(randomTopicName.substring(0, 25) + "£$$£" + randomTopicName.substring(24), aUser);
-		JMSTopic topicDupe = new JMSTopic(randomTopicName.substring(0, 25) + "£$£$%£%£" + randomTopicName.substring(24),
-				aUser);
+		String randomName = RandomStringUtils.randomAlphabetic(50);
+		JMSTopic topic = new JMSTopic(randomName.substring(0, 25) + "£$$£" + randomName.substring(24), aUser);
+		JMSTopic topicDupe = new JMSTopic(randomName.substring(0, 25) + "£$£$%£%£" + randomName.substring(24), aUser);
 		boolean expectedExceptionThrown = false;
 
 		try {
@@ -184,16 +182,16 @@ public class TopicServiceTest {
 	// Deleting topic by non owner fails
 	@Test
 	public void testDeleteTopicByNonOwnerFails() {
-		String randomTopicName = RandomStringUtils.randomAlphabetic(50);
+		String randomName = RandomStringUtils.randomAlphabetic(50);
 
-		JMSTopic topic = new JMSTopic(randomTopicName, aUser);
+		JMSTopic topic = new JMSTopic(randomName, aUser);
 		boolean expectedExceptionThrown = false;
 
 		try {
 			leases.add(userService.createDebugUser(aUser));
 			leases.add(topicService.createTopic(topic));
 
-			topicService.deleteTopic(topic, new JMSUser(randomTopicName, randomTopicName));
+			topicService.deleteTopic(topic, new JMSUser(randomName, randomName));
 
 		} catch (RemoteException | TransactionException | InvalidAttributeValueException | DuplicateEntryException e) {
 			fail("Unexpected exception thrown.");
@@ -209,9 +207,9 @@ public class TopicServiceTest {
 	// Deleting topic by owner does work
 	@Test
 	public void testDeleteTopicByOwnerAllowed() {
-		String randomTopicName = RandomStringUtils.randomAlphabetic(50);
+		String randomName = RandomStringUtils.randomAlphabetic(50);
 
-		JMSTopic topic = new JMSTopic(randomTopicName, aUser);
+		JMSTopic topic = new JMSTopic(randomName, aUser);
 
 		try {
 			leases.add(userService.createDebugUser(aUser));
@@ -229,9 +227,9 @@ public class TopicServiceTest {
 	// Deleting topic removes all messages
 	@Test
 	public void testDeletingTopicRemovesMessages() {
-		String randomTopicName = RandomStringUtils.randomAlphabetic(50);
+		String randomName = RandomStringUtils.randomAlphabetic(50);
 
-		JMSTopic topic = new JMSTopic(randomTopicName, aUser);
+		JMSTopic topic = new JMSTopic(randomName, aUser);
 
 		JMSMessage message = new JMSMessage(topic);
 		message.setFrom(aUser);
@@ -269,9 +267,9 @@ public class TopicServiceTest {
 	// Deleting topic removes all TopicUsers
 	@Test
 	public void testDeletingTopicRemovesTopicUsers() {
-		String randomTopicName = RandomStringUtils.randomAlphabetic(50);
+		String randomName = RandomStringUtils.randomAlphabetic(50);
 
-		JMSTopic topic = new JMSTopic(randomTopicName, aUser);
+		JMSTopic topic = new JMSTopic(randomName, aUser);
 
 		try {
 			leases.add(userService.createDebugUser(aUser));
@@ -297,12 +295,141 @@ public class TopicServiceTest {
 			fail("Unexpected exception thrown.");
 		}
 	}
-	
+
 	// Test getting all TopicUsers
-	
+	@Test
+	public void testGetAllTopicUsers() {
+		String randomName = RandomStringUtils.randomAlphabetic(50);
+
+		JMSTopic topic = new JMSTopic(randomName, aUser);
+		JMSUser user1 = new JMSUser(randomName + "1", randomName + "1");
+		JMSUser user2 = new JMSUser(randomName + "2", randomName + "2");
+		JMSUser user3 = new JMSUser(randomName + "3", randomName + "3");
+
+		try {
+			leases.add(userService.createDebugUser(user1));
+			leases.add(userService.createDebugUser(user2));
+			leases.add(userService.createDebugUser(user3));
+
+			leases.add(topicService.createTopic(topic));
+
+			leases.add(topicService.addDebugTopicUser(topic, user1));
+			leases.add(topicService.addDebugTopicUser(topic, user2));
+			leases.add(topicService.addDebugTopicUser(topic, user3));
+
+			JavaSpace05 space = SpaceService.getSpace();
+
+			assertEquals("Users not successfully added", 3,
+					lookupHelper.findAllMatchingTemplate(space, new JMSTopicUser(topic, null)).size());
+
+			assertEquals("Failed to get all 3 topic users", 3, topicService.getAllTopicUsers(topic).size());
+
+		} catch (RemoteException | TransactionException | InvalidAttributeValueException | DuplicateEntryException e) {
+			fail("Unexpected exception thrown.");
+		}
+	}
+
 	// Test manual TopicUser removal
-	
+	@Test
+	public void testTopicUserRemoval() {
+		String randomName = RandomStringUtils.randomAlphabetic(50);
+
+		JMSTopic topic = new JMSTopic(randomName, aUser);
+		JMSUser user1 = new JMSUser(randomName + "1", randomName + "1");
+		JMSUser user2 = new JMSUser(randomName + "2", randomName + "2");
+		JMSUser user3 = new JMSUser(randomName + "3", randomName + "3");
+
+		try {
+			leases.add(userService.createDebugUser(user1));
+			leases.add(userService.createDebugUser(user2));
+			leases.add(userService.createDebugUser(user3));
+
+			leases.add(topicService.createTopic(topic));
+
+			leases.add(topicService.addDebugTopicUser(topic, user1));
+			leases.add(topicService.addDebugTopicUser(topic, user2));
+			leases.add(topicService.addDebugTopicUser(topic, user3));
+
+			JavaSpace05 space = SpaceService.getSpace();
+
+			assertEquals("Users not successfully added", 3,
+					lookupHelper.findAllMatchingTemplate(space, new JMSTopicUser(topic, null)).size());
+
+			topicService.removeTopicUser(topic, user2);
+
+			List<JMSTopicUser> topicUsersFound = topicService.getAllTopicUsers(topic);
+			assertEquals("Failed to remove 1 topic users", 2, topicUsersFound.size());
+
+			for (int i = 0; i < topicUsersFound.size(); i++) {
+				assertFalse(topicUsersFound.get(i).getUser().getId().equals(user2.getId()));
+			}
+
+		} catch (RemoteException | TransactionException | InvalidAttributeValueException | DuplicateEntryException e) {
+			fail("Unexpected exception thrown.");
+		}
+	}
+
 	// Test get topic by id
+	@Test
+	public void testGetTopicById() {
+		String randomName = RandomStringUtils.randomAlphabetic(50);
+
+		JMSTopic topic = new JMSTopic(randomName, aUser);
+
+		try {
+			leases.add(topicService.createTopic(topic));
+			
+			JMSTopic topicFound = topicService.getTopicById(topic.getId());
+			
+			assertEquals(topic.getId(), topicFound.getId());
+		} catch (RemoteException | TransactionException | InvalidAttributeValueException | DuplicateEntryException e) {
+			fail("Unexpected exception thrown.");
+		}
+	}
+	
+	// Test get topic by base name
+	@Test
+	public void testGetTopicByBaseName() {
+		String randomName = RandomStringUtils.randomAlphabetic(50);
+
+		JMSTopic topic = new JMSTopic(randomName, aUser);
+
+		try {
+			leases.add(topicService.createTopic(topic));
+			
+			JMSTopic topicFound = topicService.getTopicByBaseName(topic.getBaseName());
+			
+			assertEquals(topic.getId(), topicFound.getId());
+		} catch (RemoteException | TransactionException | InvalidAttributeValueException | DuplicateEntryException e) {
+			fail("Unexpected exception thrown.");
+		}
+	}
 	
 	// Test getting all topics
+	@Test
+	public void testGetAllTopics() {
+		try {
+			int initialNumOfTopics = topicService.getAllTopics().size();			
+			int numTopicsToAdd = 5;
+			int numTopicsToRemove = numTopicsToAdd;
+
+			String randomName = RandomStringUtils.randomAlphabetic(50);
+			JMSTopic template = new JMSTopic(randomName, new JMSUser());
+
+			for(int i = 0; i < numTopicsToAdd; i++) {
+				leases.add(topicService.createDebugTopic(template));
+			}
+			
+			assertTrue(topicService.getAllTopics().size() == initialNumOfTopics + numTopicsToAdd);
+			
+			JavaSpace05 space = SpaceService.getSpace();
+			for(int i = 0; i < numTopicsToAdd; i++) {
+				space.takeIfExists(template, null, 500l);
+			}
+			
+			assertTrue(topicService.getAllTopics().size() == initialNumOfTopics);
+		} catch (RemoteException | TransactionException | UnusableEntryException | InterruptedException e) {
+			fail("Unexpected exception thrown.");
+		}
+	}
 }
