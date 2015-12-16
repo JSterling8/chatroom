620a55e535fc33c76b7738f7390f675fe3985918
JSterling8 <JSterling8@gmail.com>
2015-12-16 11:28:46 +0000

    Now using SHA512 instead of SHA1

--- src/services/helper/PasswordEncryptionHelper.java
+++ src/services/helper/PasswordEncryptionHelper.java
@@ -12,11 +12,12 @@ import javax.crypto.spec.PBEKeySpec;
  * 
  * Encrypts and decrypts passwords using the PBKDF2 algorithm.
  * 
- * <b>This code is not my own</b>.  It borrows heavily from https://gist.github.com/jtan189/3804290  
+ * <b>This code is not my own</b>. It borrows heavily from
+ * https://gist.github.com/jtan189/3804290
  *
  */
 public class PasswordEncryptionHelper {
-	private static final String ENCRYPTION_ALGORITHM = "PBKDF2WithHmacSHA1";
+	private static final String ENCRYPTION_ALGORITHM = "PBKDF2WithHmacSHA512";
 
 	private static final int BYTES_IN_SALT = 128;
 	private static final int BYTES_IN_HASH = 128;
@@ -29,6 +30,15 @@ public class PasswordEncryptionHelper {
 	private PasswordEncryptionHelper() {
 	}
 
+	/**
+	 * Takes in a given password and encrypts it using the "PBKDF2WithHmacSHA1"
+	 * algorithm
+	 * 
+	 * @param password
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 * @throws InvalidKeySpecException
+	 */
 	public static String encryptPassword(char[] password) throws NoSuchAlgorithmException, InvalidKeySpecException {
 		// Generate a random salt
 		SecureRandom random = new SecureRandom();
@@ -40,7 +50,8 @@ public class PasswordEncryptionHelper {
 		return toHex(salt) + ":" + ITERATIONS + ":" + toHex(hash);
 	}
 
-	private static byte[] createHash(char[] password, byte[] salt, int iterations, int bytes) throws NoSuchAlgorithmException, InvalidKeySpecException {
+	private static byte[] createHash(char[] password, byte[] salt, int iterations, int bytes)
+			throws NoSuchAlgorithmException, InvalidKeySpecException {
 		PBEKeySpec keySpec = new PBEKeySpec(password, salt, iterations, bytes * 8);
 		SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance(ENCRYPTION_ALGORITHM);
 
@@ -51,11 +62,11 @@ public class PasswordEncryptionHelper {
 			throws NoSuchAlgorithmException, InvalidKeySpecException {
 		// Decode the hash into its 3 parameters
 		String[] params = goodHash.split(":");
-		
+
 		int iterations = Integer.parseInt(params[ITERATION_INDEX]);
 		byte[] salt = fromHex(params[SALT_INDEX]);
 		byte[] hash = fromHex(params[HASH_INDEX]);
-		
+
 		// Compute the hash of the provided password, using the same salt,
 		// iteration count, and hash length
 		byte[] testHash = createHash(password, salt, iterations, hash.length);
@@ -65,11 +76,11 @@ public class PasswordEncryptionHelper {
 
 	private static boolean slowEquals(byte[] a, byte[] b) {
 		int diff = a.length ^ b.length;
-		
+
 		for (int i = 0; i < a.length && i < b.length; i++) {
 			diff |= a[i] ^ b[i];
 		}
-		
+
 		return diff == 0;
 	}
 
@@ -84,9 +95,9 @@ public class PasswordEncryptionHelper {
 	private static String toHex(byte[] array) {
 		BigInteger bi = new BigInteger(1, array);
 		String hex = bi.toString(16);
-		
+
 		int paddingLength = (array.length * 2) - hex.length();
-		
+
 		if (paddingLength > 0) {
 			return String.format("%0" + paddingLength + "d", 0) + hex;
 		} else {
