67453583c187bcfa7e7de8c238f7da4aa9b77c6b
JSterling8 <JSterling8@gmail.com>
2015-12-16 11:23:08 +0000

    Added JavaDoc to the EntryLookupHelper class

--- src/services/helper/EntryLookupHelper.java
+++ src/services/helper/EntryLookupHelper.java
@@ -29,7 +29,9 @@ public class EntryLookupHelper implements Serializable {
 	 * space.
 	 * 
 	 * Overload of the findAllMatchingTemplate(JavaSpace05 space, T template,
-	 * Transaction transaction)
+	 * Transaction transaction). Simply creates a transaction, calls the other
+	 * findAllMatchingTemplate() method, then commits the transaction before
+	 * returning the entries.
 	 * 
 	 * @param space
 	 *            The space to search for entries in.
@@ -68,14 +70,23 @@ public class EntryLookupHelper implements Serializable {
 	 */
 	@SuppressWarnings("unchecked")
 	public <T extends Entry> List<T> findAllMatchingTemplate(JavaSpace05 space, T template, Transaction transaction) {
+		// The list of entries that will eventually be returned.
 		List<T> entries = new ArrayList<T>();
 
 		try {
+			// JavaSpace05's "contents" method requires a collection of
+			// templates, so we create a collection of one element, containing
+			// only the passed-in template
 			List<T> templateList = new ArrayList<T>(1);
 			templateList.add(template);
 
+			// JavaSpace05's "contents()" method is effectively a "readAll()"
+			// method. This method is dangerous if there are a large amount of
+			// Entry objects
 			MatchSet matchSet = space.contents(templateList, transaction, 500, Long.MAX_VALUE);
 
+			// Iterate through the matchset and put each entry into the entries
+			// ArrayList
 			T entry = (T) matchSet.next();
 			while (entry != null) {
 				entries.add(entry);
@@ -91,23 +102,7 @@ public class EntryLookupHelper implements Serializable {
 				}
 			}
 
-			System.err.println("Failed to read all entries of type");
-			e.printStackTrace();
-		}
-
-		return entries;
-	}
-
-	public <T extends Entry> List<T> takeAllMatchingTemplate(JavaSpace05 space, T template) {
-		Transaction transaction = TransactionHelper.getTransaction();
-
-		List<T> entries = takeAllMatchingTemplate(space, template, transaction);
-
-		try {
-			transaction.commit();
-		} catch (UnknownTransactionException | CannotCommitException | RemoteException e) {
-			System.err.println("Failed to commit transaction.");
-
+			System.err.println("Failed to read all entries of type " + template.getClass().getSimpleName());
 			e.printStackTrace();
 		}
 
@@ -115,12 +110,47 @@ public class EntryLookupHelper implements Serializable {
 	}
 
 	/**
-	 * <b>Does not commit transaction</b>
+	 * Takes all Entries matching a given template from a given space. This
+	 * method overloads the takeAllMatchingTemplate(JavaSpace05 space, T
+	 * template, Transaction transaction) method
 	 * 
 	 * @param space
+	 *            The space to take Entries from
 	 * @param template
+	 *            The template to match Entries against
+	 * 
+	 * @return All entries that match the given template from the given space
+	 */
+	public <T extends Entry> List<T> takeAllMatchingTemplate(JavaSpace05 space, T template) {
+		try {
+			Transaction transaction = TransactionHelper.getTransaction();
+			List<T> entries = takeAllMatchingTemplate(space, template, transaction);
+			transaction.commit();
+
+			return entries;
+		} catch (UnknownTransactionException | CannotCommitException | RemoteException e) {
+			System.err.println("Failed to commit transaction.");
+			e.printStackTrace();
+
+			return new ArrayList<T>();
+		}
+
+	}
+
+	/**
+	 * Takes all Entries matching a given template from a given space, inside of
+	 * a given transaction.
+	 * 
+	 * <b>Does not commit the passed-in transaction</b>
+	 * 
+	 * @param space
+	 *            The space to take Entries from
+	 * @param template
+	 *            The template to match Entries against
 	 * @param transaction
-	 * @return
+	 *            The transaction to run the takeAll inside of
+	 * 
+	 * @return All entries that match the given template from the given space
 	 */
 	@SuppressWarnings("unchecked")
 	public <T extends Entry> List<T> takeAllMatchingTemplate(JavaSpace05 space, T template, Transaction transaction) {
