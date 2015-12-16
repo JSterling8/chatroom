cb36936f36a5cc7e40fa52130d68d8c9312a9098
JSterling8 <JSterling8@gmail.com>
2015-12-16 15:55:37 +0000

    Added setup and teardown to MessageServiceTests

--- test/tests/MessageServiceTest.java
+++ test/tests/MessageServiceTest.java
@@ -1,11 +1,12 @@
 package tests;
 
+import static org.junit.Assert.assertTrue;
+
 import java.rmi.RemoteException;
 import java.util.Date;
 import java.util.UUID;
 
-import static org.junit.Assert.*;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,7 +14,10 @@ import exceptions.ResourceNotFoundException;
 import models.JMSMessage;
 import models.JMSTopic;
 import models.JMSUser;
+import net.jini.space.JavaSpace05;
 import services.MessageService;
+import services.SpaceService;
+import services.helper.EntryLookupHelper;
 
 public class MessageServiceTest {
 
@@ -25,19 +29,33 @@ public class MessageServiceTest {
 	// Check messages in correct order
 	// Check pm only to user who is currently in topic
 
+	private JavaSpace05 space;
+	private EntryLookupHelper lookupHelper;
+	private JMSUser user;
+	private JMSTopic topic;
+	private JMSMessage message;
+	
 	@Before
 	public void setup() {
+		// Regular users can't user have special-character only names, so
+		// nothing will be duplicated. Same with topic names.
+		user = new JMSUser("$$$", "$$$");
+		topic = new JMSTopic("$$$", null);
+		message = new JMSMessage(topic, new Date(), user, null, UUID.randomUUID(), "sfsdf");
 		
+		space = SpaceService.getSpace();
+		lookupHelper = new EntryLookupHelper();
+	}
+	
+	@After
+	public void teardown() {
+		lookupHelper.takeAllMatchingTemplate(space, user);
+		lookupHelper.takeAllMatchingTemplate(space, topic);
+		lookupHelper.takeAllMatchingTemplate(space, message);
 	}
 	
 	@Test
 	public void checkNonExistantTopicCantHaveMessageSent() {
-		// Regular users can't user have special-character only names, so
-		// nothing will be duplicated. Same with topic names.
-		JMSUser user = new JMSUser("$$$", "$$$");
-		JMSTopic nonExistantTopic = new JMSTopic("$$$", null);
-
-		JMSMessage message = new JMSMessage(nonExistantTopic, new Date(), user, null, UUID.randomUUID(), "sfsdf");
 		MessageService messageService = MessageService.getMessageService();
 
 		boolean failedAsExpected = false;
